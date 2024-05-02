d61e897fee64a4c52d972bf4c8e0146232193c47
Daniela Vornic <daniela.vornic@gmail.com>
2024-05-02 22:35:03 +0300

    Fix bug for pipelines after code optimization

--- connectivity/CommandExecutor.py
+++ connectivity/CommandExecutor.py
@@ -13,6 +13,16 @@ class CommandExecutor:
         self.image_path = image_path
         self.image_format = image_path.split(".")[-1]
         self.img = None
+        self.img_basic_operations = None
+        self.img_enhancements = None
+        self.img_color_adjustments = None
+        self.img_advanced_operations = None
+
+    def update_img_operations(self):
+        self.img_basic_operations = ImgBasicOperations(self.img)
+        self.img_enhancements = ImgEnhancements(self.img)
+        self.img_color_adjustments = ImgColorAdjustments(self.img)
+        self.img_advanced_operations = ImgAdvancedOperations(self.img)
 
     def execute(self):
         img_save_loader = ImageSaveLoader(self.image_path)
@@ -21,10 +31,7 @@ class CommandExecutor:
         if not self.img:
             print("Image is None.", file=sys.stderr)
 
-        img_basic_operations = ImgBasicOperations(self.img)
-        img_enhancements = ImgEnhancements(self.img)
-        img_color_adjustments = ImgColorAdjustments(self.img)
-        img_advanced_operations = ImgAdvancedOperations(self.img)
+        self.update_img_operations()
 
         if any("convert" in command for command in self.command_results):
             convert_command = [command for command in self.command_results if "convert" in command][0]
@@ -39,76 +46,94 @@ class CommandExecutor:
 
             # Basic operations
             if command == "rotate":
-                self.img = img_basic_operations.rotate(int(parameters[0]))
+                self.img = self.img_basic_operations.rotate(int(parameters[0]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "resize":
-                self.img = img_basic_operations.resize(int(parameters[0]), int(parameters[1]))
+                self.img = self.img_basic_operations.resize(int(parameters[0]), int(parameters[1]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "crop":
-                self.img = img_basic_operations.crop(int(parameters[0]), int(parameters[1]), int(parameters[2]), int(parameters[3]))
+                self.img = self.img_basic_operations.crop(int(parameters[0]), int(parameters[1]), int(parameters[2]), int(parameters[3]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "flipX":
-                self.img = img_basic_operations.flipX()
+                self.img = self.img_basic_operations.flipX()
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "flipY":
-                self.img = img_basic_operations.flipY()
+                self.img = self.img_basic_operations.flipY()
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "convert":
-                self.img = img_basic_operations.convert(self.image_path, parameters[0])
+                self.img = self.img_basic_operations.convert(self.image_path, parameters[0])
+                self.update_img_operations()
             # Enhancements
             elif command == "blur":
-                self.img = img_enhancements.blur(int(parameters[0]))
+                self.img = self.img_enhancements.blur(int(parameters[0]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "sharpen":
-                self.img = img_enhancements.sharpen(int(parameters[0]))
+                self.img = self.img_enhancements.sharpen(int(parameters[0]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "threshold":
-                self.img = img_enhancements.threshold(int(parameters[0]))
+                self.img = self.img_enhancements.threshold(int(parameters[0]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "reduceNoise":
-                self.img = img_enhancements.reduceNoise()
+                self.img = self.img_enhancements.reduceNoise()
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             # Color adjustments
             elif command == "bw":
-                self.img = img_color_adjustments.bw()
+                self.img = self.img_color_adjustments.bw()
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "contrast":
-                self.img = img_color_adjustments.contrast(float(parameters[0]))
+                self.img = self.img_color_adjustments.contrast(float(parameters[0]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "brightness":
-                self.img = img_color_adjustments.brightness(float(parameters[0]))
+                self.img = self.img_color_adjustments.brightness(float(parameters[0]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "negative":
-                self.img = img_color_adjustments.negative()
+                self.img = self.img_color_adjustments.negative()
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             # Advanced operations
             elif command == "compress":
-                self.img = img_advanced_operations.compress(self.image_format)
+                self.img = self.img_advanced_operations.compress(self.image_format)
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "remBg":
-                self.img = img_advanced_operations.remBg()
+                self.img = self.img_advanced_operations.remBg()
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "upscale":
-                self.img = img_advanced_operations.upscale(float(parameters[0]))
+                self.img = self.img_advanced_operations.upscale(float(parameters[0]))
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
             elif command == "colorize":
-                self.img = img_advanced_operations.colorize()
+                self.img = self.img_advanced_operations.colorize()
+                self.update_img_operations()
                 if is_last_command:
                     img_save_loader.save(self.img, self.image_path)
 
