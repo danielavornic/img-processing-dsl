851ed8a223eb0cec551bec7c52891155188f898f
Daniela Vornic <daniela.vornic@gmail.com>
2024-05-02 11:15:42 +0300

    Update contents help command and add q command for testing purposes

--- main.py
+++ main.py
@@ -13,30 +13,36 @@ class Main:
         help_str = """
         Available Commands:
         - rotate --deg=<int>: Rotate the image by specified degrees.
-        - resize --w=<int> --h=<int>: Resize the image to specified width and height.
-        - crop --x=<int> --y=<int> --w=<int> --h=<int>: Crop the image to specified dimensions.
+        - resize --w=<int> --h=<int>: Resize the image to specified width and height in pixels.
+        - crop --x=<int> --y=<int> --w=<int> --h=<int>: Crop the image to specified dimensions in pixels.
         - flipX: Flip the image horizontally.
         - flipY: Flip the image vertically.
-        - convert --format=<format>: Convert the image to the specified format.
+        - convert --format=<format>: Convert the image to the specified format (png, jpg, jpeg, webp, tiff).
         - blur --lvl=<int>: Apply a blur effect with the specified radius.
         - sharpen --lvl=<int>: Apply a sharpening effect with the specified radius.
         - threshold --lvl=<int>: Apply a threshold effect with the specified value.
         - reduceNoise: Reduce noise in the image.
         - bw: Convert the image to black and white.
-        - contrast --lvl=<int>: Adjust the contrast by the specified factor.
-        - brightness --lvl=<int>: Adjust the brightness by the specified factor.
+        - contrast --lvl=<int>: Adjust the contrast by the specified factor. 100 is default.
+        - brightness --lvl=<int>: Adjust the brightness by the specified factor. 100 is default.
         - negative: Convert the image to its negative.
         - compress: Compress the image based on its format.
         - remBg: Remove the background from the image.
-        - colorize: Colorize the image.
+        - colorize: Colorize a grayscale image.
+        - upscale --lvl: Upscale the image. lvl flag is can be 2, 4, 8.
         """
         print(help_str)
 
     def run(self):
         print("Enter your command (type 'imp help' for available commands)")
+
+        # For testing purposes, run the program until the user types 'q'
         while self.running:
-            #take input in the same line as print statement
             user_input = input()
+
+            if user_input == "q":
+                self.running = False
+                break
 
             command_parser = CommandParser(user_input)
             parse_tree = command_parser.parse()
